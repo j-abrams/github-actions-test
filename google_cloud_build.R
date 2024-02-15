42c475d809b99e54118ebae378e7611019e87228
j-abrams <55149107+j-abrams@users.noreply.github.com>
2024-02-15 14:04:59 +0000

    Update google_cloud_build.R

--- google_cloud_build.R
+++ google_cloud_build.R
@@ -33,11 +33,11 @@ fields_departures <- c("airline_iata", "airline_icao", "flight_iata", "flight_ic
 
 # # 2: Airport Lookup codes
 # # Remove redundant fields with select command
-airports <- read.csv("Data/airports.csv")
+airports <- read.csv("airports.csv")
 
 # # Example 3: Airline Lookup codes
 # # Match airport code with airport name
-airlines <- read.csv("Data/airlines.csv") %>% select(-X)
+airlines <- read.csv("airlines.csv") %>% select(-X)
 
 
 
